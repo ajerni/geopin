8e82fe07039e5b72aa98d5344561c67c42e9bb1f
ajerni <ajerni@gmail.com>
2019-09-24 21:38:10 +0200

    deleted yarn.lock file

--- models/Pin.js
+++ models/Pin.js
@@ -11,7 +11,7 @@ const PinSchema = new mongoose.Schema({
         {
             text: String,
             createdAt: {type: Date, default: Date.now},
-            author: { type. mongoose.Schema.ObjectId, ref: "User"}
+            author: { type: mongoose.Schema.ObjectId, ref: "User"}
         }
     ]
 }, {timestamps: true})
